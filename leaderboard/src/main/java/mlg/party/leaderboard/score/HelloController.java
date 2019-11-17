5b8d8119bab4b53dd756b63c590cf895b15721f4
Herry <herrytco@gmail.com>
2019-11-17 16:05:04 +0100

    added CORRECT pom.xml definitons for multimodule project

--- leaderboard/src/main/java/mlg/party/leaderboard/score/HelloController.java
+++ leaderboard/src/main/java/mlg/party/leaderboard/score/HelloController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/score")
-public class ScoreController {
+@RequestMapping("/hello")
+public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping
     public String hello() {
         return "Hello World";
     }
