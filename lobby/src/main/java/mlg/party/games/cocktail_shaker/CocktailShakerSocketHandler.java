8fc9ce4a265802099d56dd1dd426514cc5310189
Herry <herrytco@gmail.com>
2020-01-17 18:18:58 +0100

    merge new stuff

--- lobby/src/main/java/mlg/party/games/cocktail_shaker/CocktailShakerSocketHandler.java
+++ lobby/src/main/java/mlg/party/games/cocktail_shaker/CocktailShakerSocketHandler.java
@@ -1,15 +1,12 @@
 package mlg.party.games.cocktail_shaker;
 
-import mlg.party.games.BasicGame;
 import mlg.party.games.GameWebSocketHandler;
 import mlg.party.games.cocktail_shaker.websocket.RequestParser;
 import mlg.party.games.cocktail_shaker.websocket.requests.CocktailShakerResult;
 import mlg.party.lobby.websocket.IRequestParser;
 import mlg.party.lobby.websocket.requests.BasicWebSocketRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Component
 public class CocktailShakerSocketHandler extends GameWebSocketHandler<CocktailShakerGame> {
@@ -18,7 +15,6 @@ public class CocktailShakerSocketHandler extends GameWebSocketHandler<CocktailSh
     protected void handleRequest(WebSocketSession session, BasicWebSocketRequest request) {
         if (request instanceof CocktailShakerResult)
             handleRequest(session, (CocktailShakerResult)request);
-
         else
             System.out.println("Did not handle message");
     }
