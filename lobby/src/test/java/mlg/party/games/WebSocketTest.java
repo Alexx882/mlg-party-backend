410b027369f357bade346451155585df971b0c25
Herry <herrytco@gmail.com>
2020-01-20 13:45:01 +0100

    added cascading testcases for:  - create lobby  - create lobby + join lobby  - create lobby + join lobby + start game

--- lobby/src/test/java/mlg/party/games/WebSocketTest.java
+++ lobby/src/test/java/mlg/party/games/WebSocketTest.java
@@ -8,6 +8,8 @@ import mlg.party.lobby.websocket.requests.JoinLobbyRequest;
 import mlg.party.lobby.websocket.requests.StartGameRequest;
 import mlg.party.lobby.websocket.responses.JoinLobbyResponse;
 import mlg.party.lobby.websocket.responses.LobbyCreatedResponse;
+import mlg.party.lobby.websocket.responses.PlayerListResponse;
+import mlg.party.lobby.websocket.responses.StartGameResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +26,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -64,12 +67,82 @@ public class WebSocketTest {
     @Test
     public void createLobbyTest() throws Exception {
         CreateLobbyRequest createLobbyRequest = new CreateLobbyRequest(executor.leader.name);
-        String reply = executor.sendMessage(createLobbyRequest, true);
+        String reply = executor.sendLeaderMessage(createLobbyRequest);
 
         LobbyCreatedResponse lobbyCreatedResponse = gson.fromJson(reply, LobbyCreatedResponse.class);
 
         assertEquals("CreateLobby", lobbyCreatedResponse.type);
     }
 
+    @Test
+    public void joinLobbyTest() throws Exception {
+        CreateLobbyRequest createLobbyRequest = new CreateLobbyRequest(executor.leader.name);
+        String reply = executor.sendLeaderMessage(createLobbyRequest);
 
+        LobbyCreatedResponse lobbyCreatedResponse = gson.fromJson(reply, LobbyCreatedResponse.class);
+
+        assertEquals("CreateLobby", lobbyCreatedResponse.type);
+
+        JoinLobbyRequest joinLobbyRequest = new JoinLobbyRequest(lobbyCreatedResponse.lobbyName, "");
+        List<String> responses = executor.sendPlayerMessage(joinLobbyRequest);
+
+        for (String response : responses) {
+            JoinLobbyResponse joinLobbyResponse = gson.fromJson(response, JoinLobbyResponse.class);
+            assertEquals("JoinLobby", joinLobbyResponse.type);
+        }
+    }
+
+    @Test
+    public void startGameTest() throws IOException {
+        CreateLobbyRequest createLobbyRequest = new CreateLobbyRequest(executor.leader.name);
+        String reply = executor.sendLeaderMessage(createLobbyRequest);
+
+        LobbyCreatedResponse lobbyCreatedResponse = gson.fromJson(reply, LobbyCreatedResponse.class);
+
+        assertEquals("CreateLobby", lobbyCreatedResponse.type);
+
+        JoinLobbyRequest joinLobbyRequest = new JoinLobbyRequest(lobbyCreatedResponse.lobbyName, "");
+        List<String> joinLobbyResponses = executor.sendPlayerMessage(joinLobbyRequest);
+
+        for (String response : joinLobbyResponses) {
+            JoinLobbyResponse joinLobbyResponse = gson.fromJson(response, JoinLobbyResponse.class);
+            assertEquals("JoinLobby", joinLobbyResponse.type);
+        }
+
+        List<String> leaderJoinLobbyResponses = executor.popAllLeaderResponses();
+        for (String response : leaderJoinLobbyResponses) {
+            PlayerListResponse joinLobbyResponse = gson.fromJson(response, PlayerListResponse.class);
+            assertEquals("PlayerJoined", joinLobbyResponse.type);
+        }
+
+        boolean emptyResponseQueue = true;
+        List<List<String>> playerJoinLobbyResponses = executor.popAllPlayerResponses();
+        for (List<String> responses : playerJoinLobbyResponses) {
+            if (responses.isEmpty()) {
+                if (emptyResponseQueue)
+                    emptyResponseQueue = false;
+                else
+                    fail("only the last player has an empty response queue after everyone joined the lobby");
+            }
+
+            for (String response : responses) {
+                JoinLobbyResponse joinLobbyResponse = gson.fromJson(response, JoinLobbyResponse.class);
+                assertEquals("PlayerJoined", joinLobbyResponse.type);
+            }
+        }
+
+        StartGameRequest startGameRequest = new StartGameRequest(lobbyCreatedResponse.lobbyName);
+        String startGameResponseString = executor.sendLeaderMessage(startGameRequest);
+
+        StartGameResponse startGameResponse = gson.fromJson(startGameResponseString, StartGameResponse.class);
+        assertEquals("StartGame", startGameResponse.type);
+
+        List<List<String>> playerStartGameResponses = executor.popAllPlayerResponses();
+        for (List<String> responses : playerStartGameResponses) {
+            for (String response : responses) {
+                StartGameResponse startGameResponsePrime = gson.fromJson(response, StartGameResponse.class);
+                assertEquals("StartGame", startGameResponsePrime.type);
+            }
+        }
+    }
 }
